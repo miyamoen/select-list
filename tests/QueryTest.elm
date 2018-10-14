a317ad45c8b10d6618f5bdb9e3410e8ed9556b62
miyamoen <akihito.biblioteca@gmail.com>
2018-10-14 16:02:42 +0900

    false test

--- tests/QueryTest.elm
+++ tests/QueryTest.elm
@@ -11,21 +11,42 @@ import Types exposing (..)
 query : Test
 query =
     describe "Query"
-        [ fuzzSegments "isHead" <|
-            \_ a after ->
-                fromLists [] a after
-                    |> isHead
-                    |> Expect.true "Expected selected is head."
-        , fuzzSegments "isLast" <|
-            \before a _ ->
-                fromLists before a []
-                    |> isLast
-                    |> Expect.true "Expected selected is last."
-        , fuzz int "isSingle" <|
-            \a ->
-                fromLists [] a []
-                    |> isSingle
-                    |> Expect.true "Expected select list has single value."
+        [ describe "isHead"
+            [ fuzzSegments "true" <|
+                \_ a after ->
+                    fromLists [] a after
+                        |> isHead
+                        |> Expect.true "Expected selected is head."
+            , fuzzSegments "false" <|
+                \before a after ->
+                    fromLists (a :: before) a after
+                        |> isHead
+                        |> Expect.false "Expected selected is not head."
+            ]
+        , describe "isLast"
+            [ fuzzSegments "true" <|
+                \before a _ ->
+                    fromLists before a []
+                        |> isLast
+                        |> Expect.true "Expected selected is last."
+            , fuzzSegments "false" <|
+                \before a after ->
+                    fromLists before a (a :: after)
+                        |> isLast
+                        |> Expect.false "Expected selected is not last."
+            ]
+        , describe "isSingle"
+            [ fuzz int "true" <|
+                \a ->
+                    fromLists [] a []
+                        |> isSingle
+                        |> Expect.true "Expected select list has single value."
+            , fuzzSegments "false" <|
+                \before a after ->
+                    fromLists before a (a :: after)
+                        |> isSingle
+                        |> Expect.false "Expected select list has some elements."
+            ]
         , fuzzSegments "length" <|
             \before a after ->
                 fromLists before a after
